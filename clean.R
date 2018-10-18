987337425cfedb725655802337923b5777a83bfc
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 13:29:17 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,3 +1,3 @@
 
-clean_data <- subset(raw_data,trimws(raw_data$Satisfaction)==c("1","1.5","2","2.5","3","3.5","4","4.5","5"))
+clean_data <- subset(raw_data,trimws(raw_data$Satisfaction)==c(1,1.5,2,2.5,3,3.5,4,4.5,5))
 clean_data
