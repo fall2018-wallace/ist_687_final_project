0cd6624fcdb8d06919f8a5dd287087d3a676c447
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 13:37:42 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,7 +1,9 @@
 
 str(raw_data)
 
-d<-raw_data[(raw_data$Satisfaction=="1" | raw_data$Satisfaction=="1.5" ),]
+d<-raw_data[(raw_data$Satisfaction=="1" | 
+            raw_data$Satisfaction=="1.5" |
+            raw_data$Satisfaction=="2" |),]
 #clean_data <- subset(raw_data,trimws(raw_data$Satisfaction)==c(1:5))
 #clean_data
 nrow(d)
