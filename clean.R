74f357f47007c4456463d752566a69809fd97bd7
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 13:46:59 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -13,3 +13,4 @@ CleanSatisfaction<-raw_data[(raw_data$Satisfaction=="1" |
 #clean_data <- subset(raw_data,trimws(raw_data$Satisfaction)==c(1:5))
 #clean_data
 nrow(CleanSatisfaction)
+omitted<-na.omit(CleanSatisfaction)
