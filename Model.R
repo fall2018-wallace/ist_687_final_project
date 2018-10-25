d987cccb3388474ec88c85def7a2ec0bd49acc37
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-25 13:16:23 -0400

    Saving Model.R

--- Model.R
+++ Model.R
@@ -3,5 +3,5 @@
 str(df)
 options(scipen=999999) # To obtain a distribution of values on the axes in readable form
 
-LM_MK<-lm(Satisfaction~Age+Price.Sensitivity,data=df)
+LM_MK<-lm(Satisfaction~Age+Airline.Status+Price.Sensitivity,data=df)
 summary(LM_MK)
