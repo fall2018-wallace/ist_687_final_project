d28b7f1f725051c0b31d8854159b24233e3889c2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-25 13:22:42 -0400

    Saving Model.R

--- Model.R
+++ Model.R
@@ -3,5 +3,5 @@
 str(df)
 options(scipen=999999) # To obtain a distribution of values on the axes in readable form
 
-LM_MK<-lm(Satisfaction~Age+Airline.Status+Price.Sensitivity+Year.of.First.Flight+No.of.Flights.p.a.+X..of.Flight.with.other.Airlines+Type.of.Travel+No..of.other.Loyalty.Cards+Shopping.Amount.at.Airport+Eating.and.Drinking.at.Airport+Class+Day.of.Month+Flight.date+Airline.Code+irline.Name+Orgin.City+Origin.State+Destination.City+estination.State+Scheduled.Departure.Hour+Departure.Delay.in.Minutes+Arrival.Delay.in.Minutes,data=df)
+LM_MK<-lm(Satisfaction~Age+Airline.Status+Price.Sensitivity+Year.of.First.Flight+No.of.Flights.p.a.+X..of.Flight.with.other.Airlines+Type.of.Travel+No..of.other.Loyalty.Cards+Shopping.Amount.at.Airport+Eating.and.Drinking.at.Airport+Class+Day.of.Month+Flight.date+Airline.Code+Airline.Name+Orgin.City+Origin.State+Destination.City+estination.State+Scheduled.Departure.Hour+Departure.Delay.in.Minutes+Arrival.Delay.in.Minutes,data=df)
 summary(LM_MK)
