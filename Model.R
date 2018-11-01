6745e1c5b8a129b592e1edb8f361b212f07bc6a2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 13:09:42 -0400

    force updated content when merge failed

--- Model.R
+++ Model.R
@@ -3,5 +3,6 @@
 str(df)
 options(scipen=999999) # To obtain a distribution of values on the axes in readable form
 
-LM_MK<-lm(Satisfaction~Age+AirlineStatus+PriceSensitivity+YearofFirstFlight+NoofFlightspa+XofFlightwithotherAirlines+TypeofTravel+NoofotherLoyaltyCards+ShoppingAmountatAirport+EatingandDrinkingatAirport+Class+DayofMonth+Flightdate+AirlineCode+ScheduledDepartureHour+DepartureDelayinMinutes+ArrivalDelayinMinutes,data=df)
-summary(LM_MK)
+#LM_MK<-lm(Satisfaction~Age+Airline.Status+Price.Sensitivity+Year.of.First.Flight+No.of.Flights.p.a.+X..of.Flight.with.other.Airlines+Type.of.Travel+No..of.other.Loyalty.Cards+Shopping.Amount.at.Airport+Eating.and.Drinking.at.Airport+Class+Day.of.Month+Flight.date+Airline.Code+Airline.Name+Orgin.City+Origin.State+Destination.City+Destination.State+Scheduled.Departure.Hour+Departure.Delay.in.Minutes+Arrival.Delay.in.Minutes,data=df)
+#summary(LM_MK)
+newcol<-na.omit(colnames(df)
