2a611d1c41030a8fd580f858ccc7c9e5c842c8b6
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 13:26:39 -0400

    Saving Insight.R

--- Insight.R
+++ Insight.R
@@ -6,7 +6,7 @@ agevssatis <- function()
 {   
     library(ggplot)
     
-    plot_comparison <- ggplot(dfinsight,aes=(x=Age, y= Satisfaction)) + geom_histogram(bins = 50, color = "red", fill ="blue")
+    plot_comparison <- ggplot(dfinsight,aes(x=Age, y= Satisfaction)) + geom_histogram(bins = 50, color = "red", fill ="blue")
     
 }
 
