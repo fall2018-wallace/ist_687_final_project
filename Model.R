e193a070ee41b7b00e2d90097100faf096e2c312
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 13:17:43 -0400

    Saving Model.R

--- Model.R
+++ Model.R
@@ -5,4 +5,5 @@ options(scipen=999999) # To obtain a distribution of values on the axes in reada
 
 LM1<-lm(Satisfaction~Age+AirlineStatus+PriceSensitivity+YearofFirstFlight+NoofFlightspa+XofFlightwithotherAirlines+TypeofTravel+NoofotherLoyaltyCards+ShoppingAmountatAirport+EatingandDrinkingatAirport+Class+DayofMonth+Flightdate+AirlineCode+ScheduledDepartureHour+DepartureDelayinMinutes+ArrivalDelayinMinutes,data=df)
 summary(LM1)
+LM2<-lm(Satisfaction~Age+AirlineStatus+PriceSensitivity+YearofFirstFlight+NoofFlightspa+XofFlightwithotherAirlines+TypeofTravel+NoofotherLoyaltyCards+ShoppingAmountatAirport+EatingandDrinkingatAirport+Class+DayofMonth+Flightdate+AirlineCode+ScheduledDepartureHour+DepartureDelayinMinutes+ArrivalDelayinMinutes,data=df)
 
