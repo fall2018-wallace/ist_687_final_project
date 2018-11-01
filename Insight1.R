c569e2b47b0b45c60a36f3c745d5866f3488f5e2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 13:33:49 -0400

    Saving Insight1.R

--- Insight1.R
+++ Insight1.R
@@ -1,10 +1,10 @@
 
 str(df)
 library("ggplot2")
-Plot1<-ggplot(df,aes(x=Age ,y=Satisfaction))+geom_point()+ggtitle("Customer Satisfaction vs Age")+xlab("Age")+ylab("Customer Satisfaction")
+#Plot1<-ggplot(df,aes(x=Age ,y=Satisfaction))+geom_point()+ggtitle("Customer Satisfaction vs Age")+xlab("Age")+ylab("Customer Satisfaction")
 
-df$Age<-as.numeric(df$Age)
+
 df$SatisfactionJ<-jitter(df$Satisfaction)
-df$AgeJ<-as.numeric(jitter(df$Age))
+df$AgeJ<-jitter(df$Age)
 
-ggplot(df,aes(x=AgeJ,y=overallCustSatJ))+geom_point()+stat_smooth(method="lm",col="red")
+Plot1<-ggplot(df,aes(x=AgeJ,y=overallCustSatJ))+geom_point()+stat_smooth(method="lm",col="red")
