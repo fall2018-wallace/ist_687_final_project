90de88a9a8d824b77f4ecb48e72df3b87176a10f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 13:37:57 -0400

    Saving Insight1.R

--- Insight1.R
+++ Insight1.R
@@ -4,8 +4,8 @@ library("ggplot2")
 #Plot1<-ggplot(df,aes(x=Age ,y=Satisfaction))+geom_point()+ggtitle("Customer Satisfaction vs Age")+xlab("Age")+ylab("Customer Satisfaction")
 
 
-df$SatisfactionJ<-jitter(df$Satisfaction)
-df$AgeJ<-as.numeric(jitter(df$Age))
+#df$SatisfactionJ<-jitter(df$Satisfaction)
+#df$AgeJ<-as.numeric(jitter(df$Age))
 
 #Plot1<-ggplot(df,aes(x=Age,y=overallCustSat))+geom_point()+stat_smooth(method="lm",col="red")
 df$Flightdate <- as.Date(df$Flightdate , "%m/%d/%y")
