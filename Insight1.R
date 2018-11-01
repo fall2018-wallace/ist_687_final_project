e7c99934932e9e7ef3e9e138f3d46ccec05af140
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 13:25:18 -0400

    Saving Insight1.R

--- Insight1.R
+++ Insight1.R
@@ -6,3 +6,5 @@ Plot1<-ggplot(df,aes(x=Age ,y=Satisfaction))+geom_point()+ggtitle("Customer Sati
 
 df$SatisfactionJ<-jitter(df$Satisfaction)
 df$AgeJ<-jitter(df$Age)
+
+ggplot(df,aes(x=AgeJ,y=overallCustSatJ))+geom_point()+stat_smooth(method="lm",col="red")
