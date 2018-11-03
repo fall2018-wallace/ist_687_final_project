2f91fefda102a4de1086053a0a50e3752643e499
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-03 14:52:39 -0400

    Saving Model.R

--- Model.R
+++ Model.R
@@ -6,6 +6,7 @@ a<-sub("yes","1",df$ArrivalDelaygreater5Mins)
 b<-sub("no","0",a)
 df$ArrivalDelaygreater5Mins<-b
 df$ArrivalDelaygreater5Mins<-as.numeric(df$ArrivalDelaygreater5Mins)
+df$ArrivalDelaygreater5Mins
 LM1<-lm(Satisfaction~Age+AirlineStatus+PriceSensitivity+YearofFirstFlight+NoofFlightspa+XofFlightwithotherAirlines+TypeofTravel+NoofotherLoyaltyCards+ShoppingAmountatAirport+EatingandDrinkingatAirport+Class+DayofMonth+Flightdate+AirlineCode+ScheduledDepartureHour+DepartureDelayinMinutes+ArrivalDelayinMinutes+Flightcancelled+Flighttimeinminutes+FlightDistance+ArrivalDelaygreater5Mins,data=df)
 summary(LM1)
 #remove the variables with P-value > 0.05.
