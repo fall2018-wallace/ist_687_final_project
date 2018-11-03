a02354284cad3cd177d2504ec8ad44e935166ed7
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-03 17:06:13 -0400

    Saving Model.R

--- Model.R
+++ Model.R
@@ -1,11 +1,7 @@
 
 str(df)
 options(scipen=999999) # To obtain a distribution of values on the axes in readable form
-a<-sub("yes","1",df$ArrivalDelaygreater5Mins)
-b<-sub("no","0",a)
-df$ArrivalDelaygreater5Mins<-b
-df$ArrivalDelaygreater5Mins<-as.numeric(df$ArrivalDelaygreater5Mins)
-df$ArrivalDelaygreater5Mins
+
 LM1<-lm(Satisfaction~Age+AirlineStatus+PriceSensitivity+YearofFirstFlight+NoofFlightspa+XofFlightwithotherAirlines+TypeofTravel+NoofotherLoyaltyCards+ShoppingAmountatAirport+EatingandDrinkingatAirport+Class+DayofMonth+Flightdate+AirlineCode+ScheduledDepartureHour+DepartureDelayinMinutes+ArrivalDelayinMinutes+Flightcancelled+Flighttimeinminutes+FlightDistance+ArrivalDelaygreater5Mins,data=df)
 summary(LM1)
 #remove the variables with P-value > 0.05.
