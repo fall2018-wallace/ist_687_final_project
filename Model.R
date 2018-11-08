339ba83c9c308773bbbe079bf50d9fbad46ae9aa
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-08 13:07:06 -0500

    Saving Model.R

--- Model.R
+++ Model.R
@@ -1,6 +1,5 @@
 
 str(df)
-View(df)
 options(scipen=999999) # To obtain a distribution of values on the axes in readable form
 df$Satisfaction<-as.numeric(df$Satisfaction)
 LM1<-lm(Satisfaction~Age+AirlineStatus+PriceSensitivity+YearofFirstFlight+NoofFlightspa+XofFlightwithotherAirlines+TypeofTravel+NoofotherLoyaltyCards+ShoppingAmountatAirport+EatingandDrinkingatAirport+Class+DayofMonth+Flightdate+AirlineCode+ScheduledDepartureHour+DepartureDelayinMinutes+ArrivalDelayinMinutes+Flighttimeinminutes+FlightDistance+ArrivalDelaygreater5Mins,data=df)
