6e80f83c58c017b38091c805c320f8fbc441333c
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-08 12:46:16 -0500

    Saving Model.R

--- Model.R
+++ Model.R
@@ -1,5 +1,6 @@
 
 str(df)
+View（df）
 options(scipen=999999) # To obtain a distribution of values on the axes in readable form
 df$Satisfaction<-as.numeric(df$Satisfaction)
 a<-sub("Yes","1",df$Flightcancelled)
