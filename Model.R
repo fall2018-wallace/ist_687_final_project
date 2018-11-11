49b64204389e61575520261e10c6d76cdf496902
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-11 14:48:12 -0500

    Saving Model.R

--- Model.R
+++ Model.R
@@ -1,7 +1,5 @@
 
-str(df)
 options(scipen=999999) # To obtain a distribution of values on the axes in readable form
-df$Satisfaction<-as.numeric(df$Satisfaction)
 LM1<-lm(Satisfaction~Age+AirlineStatus+PriceSensitivity+YearofFirstFlight+NoofFlightspa+XofFlightwithotherAirlines+TypeofTravel+NoofotherLoyaltyCards+ShoppingAmountatAirport+EatingandDrinkingatAirport+Class+DayofMonth+Flightdate+AirlineCode+ScheduledDepartureHour+DepartureDelayinMinutes+ArrivalDelayinMinutes+Flighttimeinminutes+FlightDistance+ArrivalDelaygreater5Mins,data=df)
 summary(LM1)
 #remove the variables with P-value > 0.05.
