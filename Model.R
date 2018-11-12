eec9cdceb6f4b8e6e40c5671e42606483cc03e6c
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-11 23:25:10 -0500

    Saving Model.R

--- Model.R
+++ Model.R
@@ -4,6 +4,7 @@ b <- sub("Yes","1",a)
 df$Flightcancelled <- b
 df$Flightcancelled<-as.numeric(df$Flightcancelled)
 df$Satisfaction<-as.numeric(df$Satisfaction)
+str(df$Satisfaction)
 options(scipen=999999) # To obtain a distribution of values on the axes in readable form
 str(df)
 LM1<-lm(Satisfaction~AirlineStatus+Age+Gender+PriceSensitivity+YearofFirstFlight+NoofFlightspa+XofFlightwithotherAirlines+TypeofTravel+NoofotherLoyaltyCards+ShoppingAmountatAirport+EatingandDrinkingatAirport+Class+DayofMonth+Flightdate+AirlineCode+ScheduledDepartureHour++Flightcancelled+DepartureDelayinMinutes+ArrivalDelayinMinutes+Flighttimeinminutes+FlightDistance+ArrivalDelaygreater5Mins,data=df)
