629d7249c9c46451f3415281a07007b67badb68a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-14 14:47:06 -0500

    Saving Model.R

--- Model.R
+++ Model.R
@@ -21,3 +21,30 @@ createBuckets<- function(vec){
   return(vBuckets)
 }
 
+
+
+
+
+satisfaction<-createBuckets(df$Satisfaction)
+age<-createBuckets(df$Age)
+pricesensitive<-createBuckets(df$PriceSensitivity)
+yearoffirstflight<-createBuckets(df$YearofFirstFlight)
+noofflightspa<-createBuckets(df$NoofFlightspa)
+shoppingamount<-createBuckets(df$ShoppingAmountatAirport)
+scheduleddeparturehour<-createBuckets(df$ScheduledDepartureHour)
+
+library(arules)
+library(arulesViz)
+ruleDF<- data.frame(satisfaction,df$AirlineStatus,age,df$Gender,pricesensitive,yearoffirstflight,noofflightspa,df$TypeofTravel,shoppingamount,df$Class,scheduleddeparturehour,df$ArrivalDelaygreater5Mins)
+hotelSurveyX <- as(ruleDF,"transactions")
+
+itemFrequencyPlot(hotelSurveyX,support=0.1)
+
+ruleset<- apriori(hotelSurveyX, parameter=list(support=0.1, confidence=0.5), appearance = list(default="lhs", rhs=("satisfaction=High")))
+ruleset
+
+goodruleset<-sort(ruleset,decreasing=TRUE,by="lift")[1,10]
+inspect(goodruleset)
+
+
+
