9c2454ae70917b7cff4a245b66ee49d53b71d8dd
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-21 11:46:40 -0500

    force updated content when merge failed

--- Model.R
+++ Model.R
@@ -1,12 +1,50 @@
 
+
 str(df)
 options(scipen=999999) # To obtain a distribution of values on the axes in readable form
-df$Satisfaction<-as.numeric(df$Satisfaction)
-a<-sub("Yes","1",df$Flightcancelled)
-b<-sub("No","0",a)
-df$Flightcancelled<-b
-LM1<-lm(Satisfaction~Age+AirlineStatus+PriceSensitivity+YearofFirstFlight+NoofFlightspa+XofFlightwithotherAirlines+TypeofTravel+NoofotherLoyaltyCards+ShoppingAmountatAirport+EatingandDrinkingatAirport+Class+DayofMonth+Flightdate+AirlineCode+ScheduledDepartureHour+DepartureDelayinMinutes+ArrivalDelayinMinutes+Flightcancelled+Flighttimeinminutes+FlightDistance+ArrivalDelaygreater5Mins,data=df)
+
+a <- sub("No","0",df$Flightcancelled)
+b <- sub("Yes","1",a)
+df$Flightcancelled <- b
+df$Flightcancelled<-as.numeric(df$Flightcancelled)
+
+df$Satisfaction<- as.numeric(as.character(df$Satisfaction))
+LM1<-lm(Satisfaction~AirlineStatus+Age+Gender+PriceSensitivity+YearofFirstFlight+NoofFlightspa+XofFlightwithotherAirlines+TypeofTravel+NoofotherLoyaltyCards+ShoppingAmountatAirport+EatingandDrinkingatAirport+Class+DayofMonth+Flightdate+AirlineCode+ScheduledDepartureHour++Flightcancelled+DepartureDelayinMinutes+ArrivalDelayinMinutes+Flighttimeinminutes+FlightDistance+ArrivalDelaygreater5Mins,data=df)
 summary(LM1)
-#remove the variables with P-value > 0.05.
-LM2<-lm(Satisfaction~Age+AirlineStatus+PriceSensitivity+YearofFirstFlight+NoofFlightspa+TypeofTravel+ShoppingAmountatAirport+EatingandDrinkingatAirport+Class+ScheduledDepartureHour+ArrivalDelayinMinutes,data=df)
+LM2<-lm(Satisfaction~AirlineStatus+Age+Gender+PriceSensitivity+YearofFirstFlight+NoofFlightspa+TypeofTravel+ShoppingAmountatAirport+Class+ScheduledDepartureHour+ArrivalDelaygreater5Mins,data=df)
+summary(LM2)
+createBuckets<- function(vec){
+  q <- quantile(vec, c(0.4, 0.6))
+  vBuckets <- replicate(length(vec), "Average")
+  vBuckets[vec <= q[1]] <- "Low"
+  vBuckets[vec > q[2]] <- "High"
+  return(vBuckets)
+}
+
+
+
+
+
+satisfaction<-createBuckets(df$Satisfaction)
+age<-createBuckets(df$Age)
+pricesensitive<-createBuckets(df$PriceSensitivity)
+yearoffirstflight<-createBuckets(df$YearofFirstFlight)
+noofflightspa<-createBuckets(df$NoofFlightspa)
+shoppingamount<-createBuckets(df$ShoppingAmountatAirport)
+scheduleddeparturehour<-createBuckets(df$ScheduledDepartureHour)
+
+library(arules)
+library(arulesViz)
+ruleDF<- data.frame(satisfaction,df$AirlineStatus,age,df$Gender,pricesensitive,yearoffirstflight,noofflightspa,df$TypeofTravel,shoppingamount,df$Class,scheduleddeparturehour,df$ArrivalDelaygreater5Mins)
+hotelSurveyX <- as(ruleDF,"transactions")
+
+itemFrequencyPlot(hotelSurveyX,support=0.1)
+
+ruleset<- apriori(hotelSurveyX, parameter=list(support=0.1, confidence=0.5))
+ruleset
+
+goodruleset<-sort(ruleset,decreasing=TRUE,by="lift")[1,10]
+inspect(goodruleset)
+
+
 
