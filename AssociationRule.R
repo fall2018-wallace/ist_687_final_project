52c80cb6feb924d9b7e003240be28455df81676f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-29 13:52:03 -0500

    Saving AssociationRule.R

--- AssociationRule.R
+++ AssociationRule.R
@@ -1,7 +1,7 @@
 
 # Association Rule Mining #
 
-
+# Function for mapping numeric attributes.
 createBuckets<- function(vec){
   q <- quantile(vec, c(0.4, 0.6))
   vBuckets <- replicate(length(vec), "Average")
