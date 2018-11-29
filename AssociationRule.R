69182f4836525f3fd5a982ac278e4365ca414b33
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-29 13:32:15 -0500

    Saving AssociationRule.R

--- AssociationRule.R
+++ AssociationRule.R
@@ -1,1 +1,31 @@
 
+createBuckets<- function(vec){
+  q <- quantile(vec, c(0.4, 0.6))
+  vBuckets <- replicate(length(vec), "Average")
+  vBuckets[vec <= q[1]] <- "Low"
+  vBuckets[vec > q[2]] <- "High"
+  return(vBuckets)
+}
+
+vBuckets<-replicate(length(df$Satisfaction),"Median")
+vBuckets[df$Satisfaction>3]<-"High"
+vBuckets[df$Satisfaction<3]<-"Low"
+
+Satisfaction<-as.factor(vBuckets)
+
+age<-createBuckets(df$Age)
+pricesensitive<-createBuckets(df$PriceSensitivity)
+yearoffirstflight<-createBuckets(df$YearofFirstFlight)
+noofflightspa<-createBuckets(df$NoofFlightspa)
+shoppingamount<-createBuckets(df$ShoppingAmountatAirport)
+scheduleddeparturehour<-createBuckets(df$ScheduledDepartureHour)
+
+library(arules)
+library(arulesViz)
+
+
+ruleDF<- data.frame(Satisfaction,df$AirlineStatus,age,df$Gender,pricesensitive,yearoffirstflight,noofflightspa,df$TypeofTravel,shoppingamount,df$Class,scheduleddeparturehour,df$ArrivalDelaygreater5Mins)
+hotelSurveyX<-as(ruleDF,"transactions")
+
+ruleset<- apriori(hotelSurveyX, parameter=list(support=0.1, confidence=0.5),appearance = list(default="lhs", rhs=("Satisfaction=High")))
+inspect(ruleset)
