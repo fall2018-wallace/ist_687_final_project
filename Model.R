7d644ff4f3a8fb616b3068fc9f7e330d68f3035d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-02 15:13:32 -0500

    Saving Model.R

--- Model.R
+++ Model.R
@@ -11,7 +11,7 @@ testData <- df1[randIndex[(cutPoint2_3+1):dim(df1)[1]],]
 dim(testData)
 dim(trainData)
 library(kernlab)
-svmOutput <- ksvm(df.happy ~ ., data=trainData, kernel = "rbfdot",kpar="automatic",C=5,cross=3, prob.model=TRUE)
+svmOutput <- ksvm(df.happy ~ df.Gender, data=trainData, kernel = "rbfdot",kpar="automatic",C=5,cross=3, prob.model=TRUE)
 
 svmPred <- predict(svmOutput, testData, type = "votes")
 
