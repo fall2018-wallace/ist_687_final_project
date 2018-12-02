9fe2342c4a61f32ddebec307bba86a0bd45fa9e9
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-02 13:39:33 -0500

    Saving LinearModel.R

--- LinearModel.R
+++ LinearModel.R
@@ -1,5 +1,5 @@
 
-summary(df)
+str(df)
 
 
 LM1<-lm(Satisfaction~AirlineStatus+Age+Gender+PriceSensitivity+YearofFirstFlight+NoofFlightspa+XofFlightwithotherAirlines+TypeofTravel+NoofotherLoyaltyCards+ShoppingAmountatAirport+EatingandDrinkingatAirport+Class+DayofMonth+Flightdate+AirlineCode+ScheduledDepartureHour++Flightcancelled+DepartureDelayinMinutes+ArrivalDelayinMinutes+Flighttimeinminutes+FlightDistance+ArrivalDelaygreater5Mins,data=df)
