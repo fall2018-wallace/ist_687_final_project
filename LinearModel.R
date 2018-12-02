0506b8ecff88edd135efaa274df5188376d79baa
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-02 13:42:51 -0500

    Saving LinearModel.R

--- LinearModel.R
+++ LinearModel.R
@@ -5,7 +5,8 @@ summary(df)
 # For the first model (LM1) we included all the variables for the entire data set and based on the p value that are statistically significant
 # The second model (LM2) represents a linear regression model with only the variables that we observed to be statistically significant from model 1
 # In model 2 (lm2), there are 11 variables that are stastifically signifcant and have an impact on the overall customer Satisfaction
-# Because the 11 variables is still a lot and we do not know which of the variables actually has the greatest impact on the customers satsifaction, we decided to run single linear regression models and compare the s-quares of these  models
+# Because 11 variables is still a lot and we do not know which of the variables actually have the greatest impact on customers satsifaction, we decided to run single linear regression models and compare the s-quares of these  models
+# Based on the single linear regression models, we noticed that type of travel and airline status have the greatest predicting power/impact on customer Satisfaction
 
 #Linear regression model will all the variables included in the entire dataset
 #Used the p value to cut out variables that are of no statistical value to further analysis
