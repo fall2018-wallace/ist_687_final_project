90bd696cd652354bdcb5eb2589c8476bcb5c1496
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-02 13:09:53 -0500

    Saving LinearModel.R

--- LinearModel.R
+++ LinearModel.R
@@ -1,9 +1,12 @@
 
 summary(df)
 
-#Linear model will all the variables included in the entire dataset
+#Linear regression model will all the variables included in the entire dataset
 #Used the p value to cut out variables that are of no statistical value to further analysis
+
 LM1<-lm(Satisfaction~AirlineStatus+Age+Gender+PriceSensitivity+YearofFirstFlight+NoofFlightspa+XofFlightwithotherAirlines+TypeofTravel+NoofotherLoyaltyCards+ShoppingAmountatAirport+EatingandDrinkingatAirport+Class+DayofMonth+Flightdate+AirlineCode+ScheduledDepartureHour++Flightcancelled+DepartureDelayinMinutes+ArrivalDelayinMinutes+Flighttimeinminutes+FlightDistance+ArrivalDelaygreater5Mins,data=df)
 summary(LM1)
+
+#Linear regression model with only the variables that happen to be statistically significant
 LM2<-lm(Satisfaction~AirlineStatus+Age+Gender+PriceSensitivity+YearofFirstFlight+NoofFlightspa+TypeofTravel+ShoppingAmountatAirport+Class+ScheduledDepartureHour+ArrivalDelaygreater5Mins,data=df)
 summary(LM2)
