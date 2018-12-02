2214f301571233308998f4aa9bf7e333312e3e68
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-02 14:10:45 -0500

    Saving AssociationRule.R

---
+++ AssociationRule.R
@@ -0,0 +1,2 @@
+
+ruleDF<- data.frame(Satisfaction,df$AirlineStatus,age,df$Gender,pricesensitive,yearoffirstflight,noofflightspa,df$TypeofTravel,shoppingamount,df$Class,scheduleddeparturehour,df$ArrivalDelaygreater5Mins)
