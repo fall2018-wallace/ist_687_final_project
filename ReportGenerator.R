bc6953ff41521f15c37d54bb9fbf24afe3029e4d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-03 12:10:01 -0500

    Saving ReportGenerator.R

--- ReportGenerator.R
+++ ReportGenerator.R
@@ -3,7 +3,7 @@
 fdf<-fulldf
 
 
-CompOverallSat<-aggregate(fdf[, 1], list(fulldf$AirlineName), mean)
+CompOverallSat<-aggregate(fdf[, 1], list(fdf$AirlineName), mean)
 
 
 plot1<-barplot(CompOverallSat, main="Customer Satisfaction based on Airlines", 
