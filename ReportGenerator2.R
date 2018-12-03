ea0aacb5b31d8703dd7d0320cd474ff9b8e1bd00
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-03 18:33:37 -0500

    Saving ReportGenerator2.R

--- ReportGenerator2.R
+++ ReportGenerator2.R
@@ -23,4 +23,4 @@ plot1
 
 #Customer Satisfaction based on age 
 df2<-df
-cut(df$Age, breaks=c(18, 24,30,36,42,48,54,60,66,72,78,84,90), right = FALSE)
+AgeGroups<-cut(df$Age, breaks=c(18, 24,30,36,42,48,54,60,66,72,78,84,90), right = FALSE)
