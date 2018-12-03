492ad0c3f5f924d2cb1ff9c65a227767cd23011e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-03 13:22:48 -0500

    Saving ReportGenerator2.R

--- ReportGenerator2.R
+++ ReportGenerator2.R
@@ -7,17 +7,18 @@ CustPerCity<-data.frame(table(df$OrginCity))
 # FEB. 2012 JAN. 2012 MAR. 2012 
 #         2         2         1
 CustPerCity
-colnames(CustPerCity) <- c("City", "Flights")
+colnames(CustPerCity) <- c("State", "Flights")
+gsub('.*\\,', '', CustPerCity$State)
 
 #gsub("(.*),.*", "\\1", x)
 
 library(ggplot2)
 
-CustPerCity$City <- factor(CustPerCity$City, levels = CustPerCity$City[order(CustPerCity$Flights)])
+CustPerCity$State <- factor(CustPerCity$State, levels = CustPerCity$State[order(CustPerCity$Flights)])
 
-plot1<-ggplot(CustPerCity, aes(x=City, y=Flights)) + geom_bar(stat="identity") + theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
+plot1<-ggplot(CustPerCity, aes(x=State, y=Flights)) + geom_bar(stat="identity") + theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
 plot1
 
-x <- 'Day, Bobby Jean'
-gsub('.*\\,', '', x)
+#x <- 'Day, Bobby Jean'
+#gsub('.*\\,', '', x)
 
