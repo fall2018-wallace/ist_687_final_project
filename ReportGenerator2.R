a4dd44cda844a965a2538ed3b2eaf96f93561a7f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-03 13:06:23 -0500

    Saving ReportGenerator2.R

--- ReportGenerator2.R
+++ ReportGenerator2.R
@@ -7,6 +7,7 @@ CustPerCity<-data.frame(table(df$OrginCity))
 # FEB. 2012 JAN. 2012 MAR. 2012 
 #         2         2         1
 CustPerCity
+colnames(CustPerCity) <- c("City", "Flights")
 
 #gsub("(.*),.*", "\\1", x)
 
