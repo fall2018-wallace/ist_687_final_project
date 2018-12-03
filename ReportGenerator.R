e996793e1c9d14b74f2b9cce22538962117616f4
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-03 12:28:30 -0500

    Saving ReportGenerator.R

--- ReportGenerator.R
+++ ReportGenerator.R
@@ -15,4 +15,5 @@ library(ggplot2)
 plot1<-ggplot(CompOverallSat, aes(x=Airline, y=AverageCustRating)) + geom_bar(stat="identity") + theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
 
 CompOverallSat <- CompOverallSat[order(AverageCustRating),] 
+plot2<-ggplot(CompOverallSat, aes(x=Airline, y=AverageCustRating)) + geom_bar(stat="identity") + theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
 
