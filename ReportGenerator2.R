de162ce05b3a39aa974dbe09bd71435ea656435a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-03 18:36:50 -0500

    Saving ReportGenerator2.R

--- ReportGenerator2.R
+++ ReportGenerator2.R
@@ -19,10 +19,13 @@ CustPerCity$State <- factor(CustPerCity$State, levels = CustPerCity$State[order(
 plot1<-ggplot(CustPerCity, aes(x=State, y=Flights)) + geom_bar(stat="identity",colour="white",
 fill="palevioletred3") +theme(axis.text.x = element_text(angle = 90, hjust = 1))+ ggtitle("State wise distribution
 of flights taken")+theme(plot.title= element_text(hjust=0.5))
-plot1
+#plot1
 
 #Customer Satisfaction based on age 
 df2<-df
 AgeGroups<-cut(df$Age, breaks=c(18, 24,30,36,42,48,54,60,66,72,78,84,90), right = FALSE)
 
 df2$Age<-AgeGroups
+AgeSat<-data.frame(table(df2$Age))
+
+
