54204981e714a88a6d7ab47bc3f8f3da58401a91
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-03 12:04:50 -0500

    Saving ReportGenerator.R

--- ReportGenerator.R
+++ ReportGenerator.R
@@ -3,3 +3,7 @@ summary(fulldf)
 
 
 CompOverallSat<-aggregate(fulldf[, 1], list(fulldf$AirlineName), mean)
+
+
+plot1<-barplot(CompOverallSat, main="Customer Satisfaction based on Airlines", 
+  	xlab="Airline", col="Blue")
