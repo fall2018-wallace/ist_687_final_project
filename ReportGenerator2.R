db4f076d62fdb611f28cf56d8ca3b7663763f6dd
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-03 13:04:45 -0500

    Saving ReportGenerator2.R

--- ReportGenerator2.R
+++ ReportGenerator2.R
@@ -10,7 +10,7 @@ CustPerCity
 
 #gsub("(.*),.*", "\\1", x)
 
-
+plot1<-ggplot(CompOverallSat, aes(x=Airline, y=AverageCustRating)) + geom_bar(stat="identity") + theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
 
 
 x <- 'Day, Bobby Jean'
