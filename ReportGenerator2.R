7eb2eece1715e5af86c7061553d23447e467a39b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-03 18:10:37 -0500

    Saving ReportGenerator2.R

--- ReportGenerator2.R
+++ ReportGenerator2.R
@@ -19,7 +19,9 @@ library(ggplot2)
 
 CustPerCity$State <- factor(CustPerCity$State, levels = CustPerCity$State[order(CustPerCity$Flights)])
 
-plot1<-ggplot(CustPerCity, aes(x=State, y=Flights)) + geom_bar(stat="identity") + theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
+plot1<-ggplot(CustPerCity, aes(x=State, y=Flights)) + geom_bar(stat="count",colour="white",
+fill="palevioletred3") +theme(axis.text.x = element_text(angle = 90, hjust = 1))+ ggtitle("State wise distribution
+of flights taken")+theme(plot.title= element_text(hjust=0.5))
 plot1
 
 #x <- 'Day, Bobby Jean'
