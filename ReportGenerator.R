86743b2857a50d6995e5b9e071316366811be717
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-03 12:29:14 -0500

    Saving ReportGenerator.R

--- ReportGenerator.R
+++ ReportGenerator.R
@@ -5,12 +5,12 @@ fdf<-fulldf
 
 CompOverallSat<-aggregate(fdf[, 1], list(fdf$AirlineName), mean)
 CompOverallSat<-data.frame(CompOverallSat)
-CompOverallSat
+#CompOverallSat
 colnames(CompOverallSat) <- c("Airline", "AverageCustRating")
 
 library(ggplot2)
 
-
+CompOverallSat
 
 plot1<-ggplot(CompOverallSat, aes(x=Airline, y=AverageCustRating)) + geom_bar(stat="identity") + theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
 
