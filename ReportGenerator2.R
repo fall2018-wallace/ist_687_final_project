f4fe2a5ce37e08352c7491be466efd1b6c69a352
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-03 22:31:26 -0500

    Saving ReportGenerator2.R

--- ReportGenerator2.R
+++ ReportGenerator2.R
@@ -72,6 +72,6 @@ row.names(SatState) <- NULL
 SatState$state <- NULL
 row.names(SatState) <- StateNames
 str(SatState)
-g<-ggplot(SatState,aes(x=row.names(SatState),y=sat))+geom_histogram()
+g<-ggplot(SatState,aes(x=row.names(SatState),y=sat))+geom_bar(stat="identity")
 g
 #colnames(CustPerCity) <- c("State", "Flights")
