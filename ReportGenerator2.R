e5f8c591eec675441539d5cbdc2e2915bda24f17
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-03 22:19:52 -0500

    Saving ReportGenerator2.R

--- ReportGenerator2.R
+++ ReportGenerator2.R
@@ -69,6 +69,7 @@ str(SatState)
 SatState
 StateNames<-SatState$state
 row.names(SatState) <- NULL
+-SatState$state <- NULL
 g<-ggplot(SatState,aes(x=sat,y=state))+geom_bar()
 g
 #colnames(CustPerCity) <- c("State", "Flights")
