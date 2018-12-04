7656d1fdd3203da4494686143845a73e49b9e5a3
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-03 19:04:38 -0500

    Saving ReportGenerator2.R

--- ReportGenerator2.R
+++ ReportGenerator2.R
@@ -26,8 +26,8 @@ df2<-df
 AgeGroups<-cut(df$Age, breaks=c(18, 24,30,36,42,48,54,60,66,72,78,84,90), right = FALSE)
 AgeGroups
 AgeGroups<-gsub(',', '-', AgeGroups)
-AgeGroups<-gsub('//[', '', AgeGroups)
-AgeGroups<-gsub('//)', '', AgeGroups)
+AgeGroups<-gsub('\\[', '', AgeGroups)
+AgeGroups<-gsub('\\)', '', AgeGroups)
 df2$Age<-AgeGroups
 
 AgeSat<-aggregate(df2[, 1], list(df2$Age), mean)
