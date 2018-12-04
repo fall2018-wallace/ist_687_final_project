227de433d74d9658f238eec58c6bba665be7c26d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-03 21:14:10 -0500

    Saving ReportGenerator2.R

--- ReportGenerator2.R
+++ ReportGenerator2.R
@@ -60,3 +60,6 @@ colnames(countvar) <- c("Gender", "NoOfTravelers")
 countvar
 plot3<-ggplot(countvar, aes(x=Gender, y=NoOfTravelers)) + geom_text(aes(label=NoOfTravelers), vjust=-1.0) + geom_bar(stat="identity",colour="white",fill="lightseagreen") +theme(axis.text.x = element_text(angle = 90, hjust = 1))+ ggtitle("Gender wise Customers") + theme(plot.title= element_text(hjust=0.5)) 
 
+
+
+# Airline Status and Type of travel
