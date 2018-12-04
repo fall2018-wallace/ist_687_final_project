9bbd422859a228f6772cb39dcddfc97138731c1b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-03 19:12:19 -0500

    Saving ReportGenerator2.R

--- ReportGenerator2.R
+++ ReportGenerator2.R
@@ -32,12 +32,11 @@ AgeGroups<-gsub('\\)', '', AgeGroups)
 df2$Age<-AgeGroups
 
 AgeSat<-aggregate(df2[, 1], list(df2$Age), mean)
-AgeSat
 AgeSat<-data.frame(AgeSat)
-AgeSat
+
 #CompOverallSat
 colnames(AgeSat) <- c("Age", "AverageCustRating")
-
+AgeSat
 
 plot2<-ggplot(AgeSat, aes(x=Age, y=AverageCustRating)) + geom_bar(stat="identity",colour="white",fill="blue") +theme(axis.text.x = element_text(angle = 90, hjust = 1))+ ggtitle("Age wise average Customer Satisfaction") + theme(plot.title= element_text(hjust=0.5)) 
 plot2
