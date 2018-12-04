a0f8c5060456da1132261e6dda4fb5dbdf2234e1
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-03 21:42:17 -0500

    Saving ReportGenerator2.R

--- ReportGenerator2.R
+++ ReportGenerator2.R
@@ -21,6 +21,8 @@ fill="palevioletred3") +theme(axis.text.x = element_text(angle = 90, hjust = 1))
 of flights taken")+theme(plot.title= element_text(hjust=0.5))
 #plot1
 
+#Customer satisfaction per State
+
 #Customer Satisfaction based on age 
 df2<-df
 AgeGroups<-cut(df$Age, breaks=c(18, 24,30,36,42,48,54,60,66,72,78,84,90), right = FALSE)
