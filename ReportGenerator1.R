261740a18efbf6827284db44ccf04670ac1d0fa1
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-05 21:55:16 -0500

    Saving ReportGenerator1.R

--- ReportGenerator1.R
+++ ReportGenerator1.R
@@ -19,7 +19,7 @@ plot1<-ggplot(CompOverallSat, aes(x=Airline, y=AverageCustRating)) + geom_bar(st
 CompOverallSat
 CompOverallSat$Airline <- factor(CompOverallSat$Airline, levels = CompOverallSat$Airline[order(CompOverallSat$AverageCustRating)])
 
-plot2<-ggplot(CompOverallSat, aes(x=Airline, y=AverageCustRating)) + geom_bar(stat="identity",colour="black",fill="red") + theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5)) 
+plot2<-ggplot(CompOverallSat, aes(x=Airline, y=AverageCustRating)) + geom_bar(stat="identity",colour="black",fill="cyan") + theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5)) 
 
 
 genderData<-data.frame(table(fdf$Gender))
