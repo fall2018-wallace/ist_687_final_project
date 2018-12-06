579a64e38c9df08023c48e859a09fbb3a39a7718
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-05 20:30:31 -0500

    Saving clean.R

--- clean.R
+++ clean.R
@@ -25,7 +25,7 @@ b <- sub("Yes","1",a)
 df$Flightcancelled <- b
 df$Flightcancelled<-as.numeric(df$Flightcancelled)
 
-avg(df$Age)
+average(df$Age)
 
 df$Satisfaction<- as.numeric(as.character(df$Satisfaction))
 LM1<-lm(Satisfaction~AirlineStatus+Age+Gender+PriceSensitivity+YearofFirstFlight+NoofFlightspa+XofFlightwithotherAirlines+TypeofTravel+NoofotherLoyaltyCards+ShoppingAmountatAirport+EatingandDrinkingatAirport+Class+DayofMonth+Flightdate+AirlineCode+ScheduledDepartureHour++Flightcancelled+DepartureDelayinMinutes+ArrivalDelayinMinutes+Flighttimeinminutes+FlightDistance+ArrivalDelaygreater5Mins,data=df)
