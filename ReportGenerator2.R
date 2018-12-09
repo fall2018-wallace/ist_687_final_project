2f3c1f8ba3824b8cd4ff294b1f56798f91dd5ff6
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-09 10:32:54 -0500

    Saving ReportGenerator2.R

--- ReportGenerator2.R
+++ ReportGenerator2.R
@@ -128,4 +128,5 @@ plat1Agg
 timedata<-df
 #timedata$Flighttimeinminutes
 timedata<-timedata[!(timedata$Flighttimeinminutes=="NA" & timedata$Flighttimeinminutes==0),]
-timegrp<- cut(timedata$Flighttimeinminutes, c(0:450, 50))
+timegrp<- cut(timedata$Flighttimeinminutes, breaks=c(50, 100,150,200,250,300,350,400,450), right = FALSE)
+timegrp
