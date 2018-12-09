a7d388ba2e565ba0f2dc45f4527943133f2435e9
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-09 10:26:36 -0500

    Saving ReportGenerator2.R

--- ReportGenerator2.R
+++ ReportGenerator2.R
@@ -126,6 +126,6 @@ plat1Agg<-aggregate(plat1[, 5], list(plat1$Satisfaction), mean) #PriceSensitivit
 plat1Agg
 
 timedata<-df
-max(timedata$Flighttimeinminutes)
+summary(timedata$Flighttimeinminutes)
 
 timegrp<- cut(timedata$Flighttimeinminutes, c(0:100, 10))
